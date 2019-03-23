f962f9b05c0b291b8a59276696f3a87b6d19d11d
Andersenas <stelmashyk.a@mail.ru>
2019-03-24 00:30:45 +0300

    App

---
+++ app/src/main/java/com/my_domain/notes/Note.kt
@@ -0,0 +1,9 @@
+package com.my_domain.notes
+
+class Note(nodeID: Int, nodeName: String, nodeDes: String) {
+
+    var nodeID: Int? = nodeID
+    var nodeName: String? = nodeName
+    var nodeDes: String? = nodeDes
+
+}
